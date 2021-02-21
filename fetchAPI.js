e7312b84d34b299fb34d7dd7a1e4e2e7f60df236
Girish <girishkj009@gmail.com>
2021-02-21 06:47:46 +0530

    Add files via upload

---
+++ fetchAPI.js
@@ -0,0 +1,32 @@
+// ________ examples 1
+
+/*
+fetch('https://jsonplaceholder.typicode.com/todos').then((response) => {
+    console.log('resolved:', response); //fetched the data sucessfully -- returned promise
+    return response.json();
+}).then((data) => {
+   console.log(data);   
+}).catch((err) => {
+  console.log("rejected:", err);
+})
+
+*/
+
+//___________ example 2
+
+
+fetch('http://puzzle.mead.io/puzzle?wordCount=2').then((response) => {
+  if (response.status === 200){
+      return response.json();
+  } else {
+      throw new Error('Unable to fetch the puzzle');
+  }
+}).then((data) => {
+  console.log(data);
+}).catch((error) => {
+  console.log(error);
+});
+
+
+
+
